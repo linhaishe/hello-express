314874cf1a8de1b657f2f5f414d9202d052cf817
linhaishe <xusumu@gmail.com>
2019-07-14 22:35:23 +0800

    sharetemplate

--- usage-of-ejs/server.js
+++ usage-of-ejs/server.js
@@ -17,25 +17,33 @@ app.get('/', function (req, res) {
     res.send("home page: " + req.query.find);
 });
 
-app.get('/form/:name', function (req, res) {
-    var person = req.params.name;
-    res.render('form',{person:person});
+//如何处理想要person，也想要data？处理失败
+app.get('/form1/:name', function (req, res) {
+    // var person = req.params.name;
+    // res.render('form', { person: person });
     //‘form是ejs文件名’，第一个person是自定义的，form中要使用到要传入的动态数据，第二个person是的上一行var person的person
+    var data = { age: 29, hobbie: ['eating', 'fishing', 'fighting'] };
+    res.render('form', { data: data });
+
 });
 
-app.post('/', urlencodedParser, function (req, res) {
-    console.dir(req.body);
-    res.send(req.body.name);
+app.get('/about', function (req, res) {
+    res.render('about');
 });
 
-// app.post('/upload', upload.single('logo'), function (req, res) {
-//     res.send({ 'ret_code': 0 });
-// });
+    app.post('/', urlencodedParser, function (req, res) {
+        console.dir(req.body);
+        res.send(req.body.name);
+    });
+
+    // app.post('/upload', upload.single('logo'), function (req, res) {
+    //     res.send({ 'ret_code': 0 });
+    // });
 
 
 
-app.use(express.static('./public'));
+    app.use(express.static('./public'));
 
-app.listen(3000);
+    app.listen(3000);
 
-console.log('You are listening to port 3000');
+    console.log('listening to port 3000');
