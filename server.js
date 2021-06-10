22f48b8f9a1d23cf27082a9153d994924c9e48e9
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-06-10 23:57:11 +0800

    notes

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
 //知识点：可以响应不同数据类型，JSON，数组，字符串等等
 
 //导入模块函数
-var express = require('express');
+var express = require("express");
 
 //形成实例，直接调用方法,给app赋值
 var app = express();
 
-// //定义路由，调用方法，方法不止get，还有post，delete等,get是中间件
-// app.get('/',function(req,res){
-//     //请求和响应根目录
-//     //在响应中调用一个send方法，将数据信息响应给浏览器，匿名函数，中间件。
-//     //express封装好了一些常用方法
-//     //send浏览器发送一串字符串 this is the home page，也可以发送其他数据，json，对象等
-//     //可以返回字符串数组等等，查看官方api文档
-//     res.send('this is the home page');
-// });
+//定义路由，调用方法，方法不止get，还有post，delete等,get是中间件
 
 //路由参数
 //返回json数据
-app.get('/',function(req,res){
-    //json
-    var responseObject = {
-        name: "xxxxx"
-    };
-    //数组
-    var responseObject2 = [1,2];
-    //数组里放对象
-    var responseObject3 = [{ddd:"3333"},{eee:"444"}];
-    // res.send(responseObject);
-    // res.send(responseObject2);
-    res.send(responseObject3);
-    //同等于下面那个但不必要，因为express 封装好了功能，会自动执行
-    //res.send(JSON.stringifyresponseObject)
-    //res.json(responseObject);//调用只能处理json内容的方法
+app.get("/", function (req, res) {
+  //json
+  var responseObject = {
+    name: "xxxxx",
+  };
+  //数组
+  var responseObject2 = [1, 2];
+  //数组里放对象
+  var responseObject3 = [{ ddd: "3333" }, { eee: "444" }];
+  //string
+  var responseObject4 = "this is a string";
+  // res.send(responseObject);
+  // res.send(responseObject2);
+  res.send(responseObject4);
+  //同等于下面那个但不必要，因为express 封装好了功能，会自动执行
+  //res.send(JSON.stringifyresponseObject)
+  //res.json(responseObject);//调用只能处理json内容的方法
 });
 //一次只能处理一个数据，比如responseObject,不能同时处理多个数据，该如何解决？
 
 app.listen(3000);
-console.log('listening to port 3000');
+console.log("listening to port 3000");
 //localhost:3000
-
--- usage-of-ejs/server.js
+++ usage-of-ejs/server.js
@@ -1,51 +1,46 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var multer = require('multer');
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var multer = require("multer");
 //var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 //默认将ejs文件放在同级目录views下，你也可以改名字，但是一般不建议
 //'ejs'指的是使用的模板模块名称，其他的还有'pug'等等，官方文档可查
-app.set('view engine', 'ejs');
-
-
+app.set("view engine", "ejs");
 
 var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get('/', function (req, res) {
-    console.dir(req.query);
-    res.send("home page: " + req.query.find);
+app.get("/", function (req, res) {
+  console.dir(req.query);
+  res.send("home page: " + req.query.find);
 });
 
 //如何处理想要person，也想要data？处理失败
-app.get('/form1/:name', function (req, res) {
-    // var person = req.params.name;
-    // res.render('form', { person: person });
-    //‘form是ejs文件名’，第一个person是自定义的，form中要使用到要传入的动态数据，第二个person是的上一行var person的person
-    var data = { age: 29, hobbie: ['eating', 'fishing', 'fighting'] };
-    res.render('form', { data: data });
-
+app.get("/form1/:name", function (req, res) {
+  // var person = req.params.name;
+  // res.render('form', { person: person });
+  //‘form是ejs文件名’，第一个person是自定义的，form中要使用到要传入的动态数据，第二个person是的上一行var person的person
+  var data = { age: 29, hobbie: ["eating", "fishing", "fighting"] };
+  res.render("form", { data: data });
 });
 
-app.get('/about', function (req, res) {
-    res.render('about');
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
-    app.post('/', urlencodedParser, function (req, res) {
-        console.dir(req.body);
-        res.send(req.body.name);
-    });
+app.post("/", urlencodedParser, function (req, res) {
+  console.dir(req.body);
+  res.send(req.body.name);
+});
 
-    // app.post('/upload', upload.single('logo'), function (req, res) {
-    //     res.send({ 'ret_code': 0 });
-    // });
+// app.post('/upload', upload.single('logo'), function (req, res) {
+//     res.send({ 'ret_code': 0 });
+// });
 
+app.use(express.static("./public"));
 
+app.listen(3000);
 
-    app.use(express.static('./public'));
-
-    app.listen(3000);
-
-    console.log('listening to port 3000');
+console.log("listening to port 3000");
