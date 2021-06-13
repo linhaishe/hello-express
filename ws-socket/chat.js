41a9f6b0d4df141e4840b50cb03cb60b6ead0aad
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-06-13 20:11:23 +0800

    websocket

--- ws-socket/chat.js
+++ ws-socket/chat.js
@@ -4,16 +4,21 @@ var oInp = document.querySelector("input");
 var oBtn = document.querySelector("button");
 var oA = document.querySelector("a");
 
+//这里还没有写注册和登入的功能，我们先直接进入websocket页面
 //登录成功才会进这个页面。进了这个页面用户信息已经有了。
-//一进来就链接
-var socket = new WebSocket("ws://172.17.7.223:9999");
+//一进来就链接我的服务器,new出服务接口
+//http://172.17.7.223,这里创建的就是ws协议而不是http协议了
+var socket = new WebSocket("ws://172.17.7.249:9999");
 //发送消息
 oBtn.onclick = function () {
+  //正常情况下最好能有用户的id，因为用户名是可以重复的
   var msg = {
     name: oS.innerHTML,
     con: oInp.value,
   };
-  socket.send(JSON.stringify(msg)); //发送的是字符串
+  //有人发消息
+  //所有发送的东西都是字符串
+  socket.send(JSON.stringify(msg));
 };
 //接收消息
 socket.onopen = function () {
@@ -22,6 +27,7 @@ socket.onopen = function () {
   socket.onmessage = function (data) {
     // data里面的data才是数据
     console.log(data.data);
+    //因为获得的是字符串，则用parse解析成json格式
     var json = JSON.parse(data.data);
     var oP = document.createElement("p");
     if (json.name == "heather") {
--- ws-socket/server.js
+++ ws-socket/server.js
@@ -1,66 +1,23 @@
 //引入express
 var express = require("express");
-//引入图标
-var favicon = require("serve-favicon");
-//引入日志
-var morgan = require("morgan");
+
 //引入post请求配置文件
 var bodyParser = require("body-parser");
-//引入session
-var session = require("express-session");
-//引入路由
-var loginRouter = require("./router/login");
-var liuyanbanRouter = require("./router/liuyanban");
-var dormRouter = require("./router/dorm");
-var file = require("./router/file");
+
 // 引入自带 路径模块
 var path = require("path");
 const { request } = require("express");
-//console.log(__dirname)
-//console.log(path.join(__dirname,'http',favicon.ico))
-//利用express创建服务器 app就是服务器
+
 var app = express();
-//配置静态文件目录
-app.use(express.static("http"));
-//配置图标
-app.use(favicon("./http/favicon.ico"));
-//配置日志
-app.use(morgan("dev"));
-//配置post请求
+
 //设置post为from表单类型
 app.use(bodyParser.urlencoded({ extended: false }));
 //设置数据的格式是json格式
 app.use(bodyParser.json());
 //配置session
-app.use(
-  session({
-    secret: "web258", // session的签名
-    name: "aaa",
-    resave: true, //重新保存
-    rolling: true, //更新过期时间
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }, //设置过期时间  默认毫秒是单位
-  })
-);
-//接口拦截
-app.all("*", function (req, res, next) {
-  console.log("session", req.session.user);
-  console.log("url", req.url);
-  if (req.session.user || req.url.split("?")[0] == "/login") {
-    next();
-  } else {
-    res.json({ error: 2, msh: "请登录" }); //重定向到登录页面
-  }
-});
-
-//接口配置
-app.use(loginRouter);
-app.use(liuyanbanRouter);
-app.use(dormRouter);
-app.use(file);
 
 //配置端口号
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log("项目启动了");
 });
 //-----------ws----------------------//
@@ -71,23 +28,29 @@ var server = new ws.Server({
   host: "172.17.7.249",
   port: 9999,
 });
+//这个数组存储链接我的人
 var arr = [];
 //当有人和我建立关联的时候触发
+//on方法,
+//传入的参数，ws为当前链接的对象
 server.on("connection", function (ws) {
-  //ws当前链接的对象
+  //ws当前链接的对象，一有人进入服务器后(ws://172.17.7.249:9999)就会到这步骤
   console.log("有人进来了");
+  //将链接进来的人存储起来
   arr.push(ws);
 
-  //当有人发送消息的时候
+  //当有人发送消息的时候，使用ws里的获取消息的方法
+
   ws.on("message", function (data) {
     //data就是发送过来的消息
     console.log("接收到的数据", data);
     for (var i = 0; i < arr.length; i++) {
-      //给别人发送消息
+      //接收到的信息发送给别人
       arr[i].send(data);
     }
   });
-  ////断开链接
+
+  //断开链接
   ws.on("close", function () {
     for (var i = 0; i < arr.length; i++) {
       if (arr[i] == ws) {
